848fdb97a7cdb6d9ff8e411affa0516d8f196314
shaief <shaief@gmail.com>
2013-12-09 13:16:35 +0200

    first commit of WordWiki

---
+++ src/pages/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from pages.models import Page
+
+admin.site.register(Page)
+
+# Register your models here.
---
+++ src/pages/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Page(models.Model):
+    word = models.CharField(max_length=200)
+    definition = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.question
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
